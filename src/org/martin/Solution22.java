3557728d9d93edff399114c1f80c827ef5d3df6e
martin <martinkingwang@gmail.com>
2020-04-19 16:50:55 +0800

    4.19

--- src/org/martin/Solution22.java
+++ src/org/martin/Solution22.java
@@ -2,10 +2,20 @@ package org.martin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution22 {
     public List<String> generateParenthesis(int n) {
         List<String> results = new ArrayList<>();
+        List<Integer> stack = new ArrayList<>();
+        for(int i = 0; i < n * 2; i ++){
+            for(int j = 0; j < results.size(); j ++){
+                
+            }
+            if(results.size() == 0){
+                results.add("(");
+            }
+        }
         return results;
     }
 }
